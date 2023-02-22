e125bc63365051df28794f577405bab6a0a7464b
Jamie Temple <jamie-temple@live.de>
2023-02-22 17:46:29 +0100

    Add draw functionality

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
+
 const canvas = document.querySelector("section[title='canvas']");
 for (let index = 0; index < 256; index++) {
     const pixel = document.createElement("div");
     pixel.classList.add("pixel");
     pixel.setAttribute("title", "");
     canvas.appendChild(pixel);
+
+
+    let change = () => pixel.style["backgroundColor"] = "#eb5e28";
+
+    pixel.addEventListener("mouseover", (event) => {
+        if (event.buttons == 1) {
+            change();
+        }
+    });
+
+    pixel.addEventListener("mousedown", (event) => {
+        change();
+    })
 }
